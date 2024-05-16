d8b60ba291bb9f14b0df6b463789e16d5ecdb4cf
Adhri <adhri.adhri@dentsu.com>
2024-05-17 01:49:09 +0700

    add middleware upload file

--- app/Controllers/user.js
+++ app/Controllers/user.js
@@ -1,7 +1,11 @@
 const userModel = require("../Models/User");
+const path = require('path');
+const fs = require('fs');
+
+const rootDir = path.resolve(__dirname, '../../');
 
 module.exports = {
-    getProfile: async (req, res) => {
+    getProfile: async (req, res, next) => {
         try {
             return res.status(200).json({
                 status: 200,
@@ -30,6 +34,40 @@ module.exports = {
                 });
             }
 
+            if (req.file) {
+                const temp_path = req.file.path;
+                const originalExt = req.file.originalname.split(".")[
+                    req.file.originalname.split(".").length - 1
+                ];
+                let filename = req.file.filename + "." + originalExt;
+                const target_path = path.resolve(rootDir, `public/uploads/${filename}`);
+
+                const src = fs.createReadStream(temp_path);
+                const dest = fs.createWriteStream(target_path);
+
+                src.pipe(dest);
+                src.on('end', async () => {
+                    const current_image = `${rootDir}public/uploads/${user.image}`;
+
+                    if (fs.existsSync(current_image)) {
+                        fs.unlinkSync(current_image);
+                    }
+
+                    updateUser = await userModel.findOneAndUpdate({ _id: user._id }, { image: filename, fullName, username });
+
+                    req.user = updateUser;
+                    return res.status(200).json({
+                        status: 200,
+                        message: 'Update profile successfully!',
+                        data: updateUser
+                    });
+                });
+                src.on('error', () => {
+                    next();
+                });
+                return;
+            }
+
             updateUser = await userModel.findOneAndUpdate({ _id: user._id }, { fullName, username });
 
             delete updateUser._doc.password;
@@ -38,6 +76,7 @@ module.exports = {
             return res.status(200).json({
                 status: 200,
                 message: 'Update profile successfully!',
+                data: updateUser
             })
 
         } catch (error) {
--- app/Routes/user.js
+++ app/Routes/user.js
@@ -1,9 +1,11 @@
 const { getProfile, updateProfile } = require('../Controllers');
 const authMiddleware = require('../Middleware/auth');
+const multer = require('multer');
+const os = require('os');
 
 const router = require('express').Router();
 
 router.get('/', [authMiddleware], getProfile);
-router.post('/update-profile', [authMiddleware], updateProfile);
+router.post('/update-profile', [authMiddleware, multer({ dest: os.tmpdir() }).single('image')], updateProfile);
 
 module.exports = router;
