a9fa82f41043fea507cfa4af12a7c95a185de67b
Adhri <adhri.adhri@dentsu.com>
2024-05-19 21:05:25 +0700

    update

--- app/Utils/adminFirebase.js
+++ app/Utils/adminFirebase.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../../serviceAccount.json");
+var serviceAccount = require("../../google_services.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
