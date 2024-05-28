dd6d781720e22c12d6080d82d89e207bed7928b2
Adhri <adhri.adhri@dentsu.com>
2024-05-28 13:00:06 +0700

    update

--- app/Routes/testing.js
+++ app/Routes/testing.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({
+    console.log('masuk route /');
+    return res.status(200).json({
         message: 'API Mobile Chat App',
     });
 });
